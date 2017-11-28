7761bafc19cad25d2b7a4ec95a40f96a708a1405
yohanei <sleepcat10235269@gmail.com>
2017-11-28 11:31:43 +0800

    login test

--- app/src/main/java/com/tom/atm/LoginActivity.java
+++ app/src/main/java/com/tom/atm/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.tom.atm;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,10 +16,23 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void login(View view){
-
+        EditText edUserid = (EditText) findViewById(R.id.userid);
+        EditText edPasswd = (EditText) findViewById(R.id.passwd);
+        String uid = edUserid.getText().toString();
+        String pw = edPasswd.getText().toString();
+        if(uid.equals("jack") && pw.equals("1234")){
+            Toast.makeText(this , "登入成功" , Toast.LENGTH_LONG).show();
+            finish();
+        }else{
+            new AlertDialog.Builder(this)
+                    .setTitle("登入")
+                    .setMessage("登入失敗")
+                    .setPositiveButton("OK" ,null)
+                    .show();
+        }
     }
 
     public void cancel(View view){
-        
+
     }
 }
